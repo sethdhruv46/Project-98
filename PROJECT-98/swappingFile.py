6a06c7be2f46c546f56ed86ef4209bd7451e6dd0
Dhruv Seth <79201843+sethdhruv46@users.noreply.github.com>
2022-06-07 17:49:37 +0530

    Add files via upload

---
+++ PROJECT-98/swappingFile.py
@@ -0,0 +1,10 @@
+def swappingFiles():
+    with open("file1.txt", 'r') as a:
+        data_a = a.read()
+    with open("file2.txt", 'r') as b:
+        data_b = b.read()    
+    with open("file1.txt", 'w') as a:
+        a.write(data_b)    
+    with open("file2.txt", 'w') as b:
+        b.write(data_a)
+swappingFiles()
